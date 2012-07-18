7b7cf021ceb2a33e3558a8d53e77a01aa8e73c0f
Karl Macklin <tacklemcclean@gmail.com>
2012-07-18 19:35:10 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 TailGate
 ========
 
-node.js &amp; socket.io file watcher for tailing multiple files on server
+node.js file watcher for tail-following log files on server.
+Using socket.io, jQuery, Twitter Bootstrap.
 
-under construction - learning git/github, nothing to see here yet!
+Usage:
+- Run "node tailgate.js"
+- Default port is 8081
+- Connect to the server, ex: http://dev.yourserver.com:8081
+- Enter full path to the file you wish to tail
+
+Bonus stuff:
+- You can turn off the autoscrolling
+- Cookies will save a history of the files you tail
+- Cookie can be cleared with "Clear file history cookie" button
+- Works in multiple browser tabs, updates dropdown history on every new file connect
+
+You can easily change the buffersize in tailgate.js if you wish.
+Only tested on Debian Linux, relies heavily on Node.js's fs.watch function which may or may not work depending on your system.
 
 
-viktigt!!
-
+Software released under MIT license, see the long text in license.txt
--- tailgate.js
+++ tailgate.js
@@ -1,7 +1,88 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: Kalle
- * Date: 2012-06-17
- * Time: 16:22
- * To change this template use File | Settings | File Templates.
- */
+var app = require('http').createServer(handler),
+	io = require('socket.io').listen(app, {log:false}),
+	fs = require('fs');
+
+app.listen(8081);
+
+var maxBuffer = 20000;
+
+function handler (req, res) {
+	fs.readFile(__dirname + '/html/index.html',
+		function (err, data) {
+			if (err) {
+				res.writeHead(500);
+				return res.end('Error loading index.html');
+			}
+			res.writeHead(200);
+			res.end(data);
+		});
+}
+
+io.sockets.on('connection', function (socket) {
+	var currentWatcher;
+	var currentFilesize;
+
+	socket.emit('connected');
+
+	socket.on('openFile', function (data) {
+		fs.stat(data.filename, function(err, stat){
+
+			if(currentWatcher)
+				currentWatcher.close();
+
+			if(err) {
+				console.log(err);
+				socket.emit('error', err.toString());
+				return;
+			}
+
+			currentFilesize = stat.size;
+
+			var start = (stat.size > maxBuffer)?(stat.size - maxBuffer):0;
+			var stream = fs.createReadStream(data.filename,{start:start, end:stat.size});
+			stream.addListener("error",function(err){
+				socket.emit('error', err.toString());
+			});
+			stream.addListener("data", function(filedata){
+				filedata = filedata.toString('utf-8');
+				var lines;
+				if(filedata.length >= maxBuffer){
+					lines = filedata.slice(filedata.indexOf("\n")+1).split("\n");
+				} else {
+					lines = filedata.split("\n");
+				}
+				socket.emit('initialTextData',{ text : lines, filename: data.filename});
+				startWatching(data.filename, socket);
+				console.log('started watching '+data.filename);
+			});
+		});
+	});
+
+
+	function startWatching(filename, socket) {
+		currentWatcher = fs.watch(filename, function(event){
+			fs.stat(filename, function(err,stat){
+				if(err) {
+					console.log(err);
+					socket.emit('error', err.toString());
+					return;
+				}
+				if(currentFilesize > stat.size) {
+					socket.emit('fileReset',{filename:filename});
+					currentFilesize = stat.size;
+					return;
+				}
+				var stream = fs.createReadStream(filename, { start: currentFilesize, end: stat.size});
+				stream.addListener("error",function(err){
+					socket.emit('error', err.toString());
+				});
+				stream.addListener("data", function(filedata) {
+					socket.emit('continuousTextData',{ text : filedata.toString('utf-8').split("\n") });
+					currentFilesize = stat.size;
+				});
+			});
+		});
+	}
+
+});
+
